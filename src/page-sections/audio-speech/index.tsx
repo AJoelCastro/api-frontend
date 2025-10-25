3dbc0b77dee0e989cb925e8bb7277d0510ab070c
AJoelCastro <misthorn33@gmail.com>
2025-10-25 10:22:58 -0500

    index carpets

---
+++ src/page-sections/audio-speech/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AudioSpeechPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AudioSpeechPage
---
+++ src/page-sections/images-vision/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImagesVisionPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ImagesVisionPage
